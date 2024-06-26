e0e71cbc7b9d1a1e16eeeb95fb2544929b3838a1
ZubkoDaryna <124194246+ZubkoDaryna@users.noreply.github.com>
2024-06-26 23:50:09 +0300

    Add files via upload
    Lab4

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.comboBoxTables = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnSearch = new System.Windows.Forms.Button(); 
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Location = new System.Drawing.Point(12, 41);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.Size = new System.Drawing.Size(776, 330);
+            this.dataGridView.TabIndex = 0;
+            
+            this.btnAdd.Location = new System.Drawing.Point(12, 12);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 1;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            
+            this.btnEdit.Location = new System.Drawing.Point(93, 12);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(75, 23);
+            this.btnEdit.TabIndex = 2;
+            this.btnEdit.Text = "Edit";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
+           
+            this.btnDelete.Location = new System.Drawing.Point(174, 12);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 3;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            
+            this.comboBoxTables.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxTables.FormattingEnabled = true;
+            this.comboBoxTables.Items.AddRange(new object[] {
+            "GraphicAdapters",
+            "CdDevices",
+            "HardDrives"});
+            this.comboBoxTables.Location = new System.Drawing.Point(370, 14);
+            this.comboBoxTables.Name = "comboBoxTables";
+            this.comboBoxTables.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxTables.TabIndex = 4;
+            this.comboBoxTables.SelectedIndexChanged += new System.EventHandler(this.comboBoxTables_SelectedIndexChanged);
+           
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(330, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Table: ";
+           
+            this.btnSearch.Location = new System.Drawing.Point(255, 12);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 6;
+            this.btnSearch.Text = "Search"; 
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click); 
+            
+
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSearch); 
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBoxTables);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.dataGridView);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.ComboBox comboBoxTables;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnSearch; 
+        private System.ComponentModel.IContainer components;
+    }
+}
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Data;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private MySqlConnection connection;
+        private string connectionString = "server=localhost;user=root;database=computeraccessoriesdb;port=3306;password=Jkweko90-";
+
+        public Form1()
+        {
+            InitializeComponent();
+            connection = new MySqlConnection(connectionString);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBoxTables.Items.AddRange(new string[] { "GraphicAdapters", "CdDevices", "HardDrives" });
+            comboBoxTables.SelectedIndex = 0;
+            LoadData(comboBoxTables.SelectedItem.ToString());
+        }
+
+        private void LoadData(string tableName)
+        {
+            string query = $"SELECT * FROM {tableName}";
+            MySqlDataAdapter adapter = new MySqlDataAdapter(query, connection);
+            DataTable table = new DataTable();
+            adapter.Fill(table);
+            dataGridView.DataSource = table;
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            EditRecord(comboBoxTables.SelectedItem.ToString());
+        }
+
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+            if (dataGridView.SelectedRows.Count > 0)
+            {
+                int id = Convert.ToInt32(dataGridView.SelectedRows[0].Cells[0].Value);
+                EditRecord(comboBoxTables.SelectedItem.ToString(), id);
+            }
+        }
+
+        private void EditRecord(string tableName, int? id = null)
+        {
+            string type = "", manufacturer = "", capacity = "", frequency = "";
+            int memory = 0;
+
+            if (id.HasValue)
+            {
+                string query = $"SELECT * FROM {tableName} WHERE id = {id}";
+                MySqlCommand command = new MySqlCommand(query, connection);
+                connection.Open();
+                MySqlDataReader reader = command.ExecuteReader();
+                if (reader.Read())
+                {
+                    type = reader["type"].ToString();
+                    manufacturer = reader["manufacturer"].ToString();
+                    if (tableName == "GraphicAdapters")
+                        memory = Convert.ToInt32(reader["memory"]);
+                    else if (tableName == "HardDrives")
+                        capacity = reader["capacity"].ToString();
+                    else if (tableName == "CdDevices")
+                        frequency = reader["frequency"].ToString();
+                }
+                connection.Close();
+            }
+
+            Form editForm = new Form();
+            TextBox txtType = new TextBox() { Text = type, Top = 10, Left = 100, Width = 200 };
+            TextBox txtManufacturer = new TextBox() { Text = manufacturer, Top = 40, Left = 100, Width = 200 };
+            TextBox txtMemory = null;
+            TextBox txtCapacity = null;
+            TextBox txtFrequency = null;
+
+            if (tableName == "GraphicAdapters")
+                txtMemory = new TextBox() { Text = memory.ToString(), Top = 70, Left = 100, Width = 200 };
+            else if (tableName == "HardDrives")
+                txtCapacity = new TextBox() { Text = capacity, Top = 70, Left = 100, Width = 200 };
+            else if (tableName == "CdDevices")
+                txtFrequency = new TextBox() { Text = frequency, Top = 70, Left = 100, Width = 200 };
+
+            Button btnSave = new Button() { Text = "Save", Top = 100, Left = 100 };
+            btnSave.Click += (s, ev) =>
+            {
+                type = txtType.Text;
+                manufacturer = txtManufacturer.Text;
+                if (tableName == "GraphicAdapters")
+                    memory = Convert.ToInt32(txtMemory.Text);
+                else if (tableName == "HardDrives")
+                    capacity = txtCapacity.Text;
+                else if (tableName == "CdDevices")
+                    frequency = txtFrequency.Text;
+
+                string query;
+                if (id.HasValue)
+                {
+                    if (tableName == "GraphicAdapters")
+                        query = $"UPDATE {tableName} SET type = '{type}', manufacturer = '{manufacturer}', memory = {memory} WHERE id = {id}";
+                    else if (tableName == "HardDrives")
+                        query = $"UPDATE {tableName} SET type = '{type}', manufacturer = '{manufacturer}', capacity = '{capacity}' WHERE id = {id}";
+                    else if (tableName == "CdDevices")
+                        query = $"UPDATE {tableName} SET type = '{type}', manufacturer = '{manufacturer}', frequency = '{frequency}' WHERE id = {id}";
+                    else
+                        query = $"UPDATE {tableName} SET type = '{type}', manufacturer = '{manufacturer}' WHERE id = {id}";
+                }
+                else
+                {
+                    if (tableName == "GraphicAdapters")
+                        query = $"INSERT INTO {tableName} (type, manufacturer, memory) VALUES ('{type}', '{manufacturer}', {memory})";
+                    else if (tableName == "HardDrives")
+                        query = $"INSERT INTO {tableName} (type, manufacturer, capacity) VALUES ('{type}', '{manufacturer}', '{capacity}')";
+                    else if (tableName == "CdDevices")
+                        query = $"INSERT INTO {tableName} (type, manufacturer, frequency) VALUES ('{type}', '{manufacturer}', '{frequency}')";
+                    else
+                        query = $"INSERT INTO {tableName} (type, manufacturer) VALUES ('{type}', '{manufacturer}')";
+                }
+
+                MySqlCommand command = new MySqlCommand(query, connection);
+                connection.Open();
+                command.ExecuteNonQuery();
+                connection.Close();
+                editForm.Close();
+                LoadData(tableName);
+            };
+
+            editForm.Controls.Add(new Label() { Text = "Type", Top = 10, Left = 10 });
+            editForm.Controls.Add(txtType);
+            editForm.Controls.Add(new Label() { Text = "Manufacturer", Top = 40, Left = 10 });
+            editForm.Controls.Add(txtManufacturer);
+            if (tableName == "GraphicAdapters")
+            {
+                editForm.Controls.Add(new Label() { Text = "Memory", Top = 70, Left = 10 });
+                editForm.Controls.Add(txtMemory);
+            }
+            else if (tableName == "HardDrives")
+            {
+                editForm.Controls.Add(new Label() { Text = "Capacity", Top = 70, Left = 10 });
+                editForm.Controls.Add(txtCapacity);
+            }
+            else if (tableName == "CdDevices")
+            {
+                editForm.Controls.Add(new Label() { Text = "Frequency", Top = 70, Left = 10 });
+                editForm.Controls.Add(txtFrequency);
+            }
+            editForm.Controls.Add(btnSave);
+            editForm.ShowDialog();
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (dataGridView.SelectedRows.Count > 0)
+            {
+                int id = Convert.ToInt32(dataGridView.SelectedRows[0].Cells[0].Value);
+                string tableName = comboBoxTables.SelectedItem.ToString();
+                DeleteRecord(id, tableName);
+            }
+        }
+
+        private void DeleteRecord(int id, string tableName)
+        {
+            string query = $"DELETE FROM {tableName} WHERE id = {id}";
+            MySqlCommand command = new MySqlCommand(query, connection);
+            connection.Open();
+            command.ExecuteNonQuery();
+            connection.Close();
+            LoadData(tableName);
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            string tableName = comboBoxTables.SelectedItem.ToString();
+            Search(tableName);
+        }
+
+        private void Search(string tableName)
+        {
+            Form searchForm = new Form();
+            TextBox txtType = new TextBox() { Top = 10, Left = 100, Width = 200 };
+            TextBox txtManufacturer = new TextBox() { Top = 40, Left = 100, Width = 200 };
+            TextBox txtCapacity = null; 
+            TextBox txtFrequency = null; 
+
+            Button btnSearch = new Button() { Text = "Search", Top = 70, Left = 100 };
+            btnSearch.Click += (s, ev) =>
+            {
+                string type = txtType.Text;
+                string manufacturer = txtManufacturer.Text;
+                string capacity = txtCapacity?.Text ?? ""; 
+                string frequency = txtFrequency?.Text ?? ""; 
+
+                string query = $"SELECT * FROM {tableName} WHERE type LIKE '%{type}%' AND manufacturer LIKE '%{manufacturer}%'";
+
+                if (tableName == "HardDrives")
+                {
+                    query += $" AND capacity LIKE '%{capacity}%'";
+                }
+                else if (tableName == "CdDevices")
+                {
+                    query += $" AND frequency LIKE '%{frequency}%'";
+                }
+
+                MySqlDataAdapter adapter = new MySqlDataAdapter(query, connection);
+                DataTable table = new DataTable();
+                adapter.Fill(table);
+                dataGridView.DataSource = table;
+                searchForm.Close();
+            };
+
+            searchForm.Controls.Add(new Label() { Text = "Type", Top = 10, Left = 10 });
+            searchForm.Controls.Add(txtType);
+            searchForm.Controls.Add(new Label() { Text = "Manufacturer", Top = 40, Left = 10 });
+            searchForm.Controls.Add(txtManufacturer);
+
+            if (tableName == "HardDrives")
+            {
+                txtCapacity = new TextBox() { Top = 70, Left = 100, Width = 200 };
+                searchForm.Controls.Add(new Label() { Text = "Capacity", Top = 70, Left = 10 });
+                searchForm.Controls.Add(txtCapacity);
+            }
+            else if (tableName == "CdDevices")
+            {
+                txtFrequency = new TextBox() { Top = 70, Left = 100, Width = 200 };
+                searchForm.Controls.Add(new Label() { Text = "Frequency", Top = 70, Left = 10 });
+                searchForm.Controls.Add(txtFrequency);
+            }
+
+            searchForm.Controls.Add(btnSearch);
+            searchForm.ShowDialog();
+        }
+
+        private void comboBoxTables_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string tableName = comboBoxTables.SelectedItem.ToString();
+            LoadData(tableName);
+        }
+    }
+}
